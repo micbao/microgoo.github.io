4c33ed8958dbafa3f25c7d236d80a75b99523949
xiong <xb_win@163.com>
2017-09-18 12:55:42 +0800

    test

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,33 @@
 (function($) {
 	$(document).ready(function() {
 		init();
+
+		$.indexedDB("databaseName", {
+			"schema": {
+				"1": function(versionTransaction) {
+					versionTransaction.createObjectStore("objectStore1");
+				},
+				"2": function(versionTransaction) {
+					versionTransaction.createObjectStore("objectStore2");
+				}
+			}
+		}).transaction(["objectStore1", "objectStore2"]).then(function() {
+			log("Transaction completed");
+		}, function() {
+			log("Transaction aborted");
+		}, function(t) {
+			log("Transaction in progress");
+			t.objectStore("objectStore1").add({
+				"valueProp": "val",
+				"anotherProp": 2
+			}, 1).then(function() {
+				log("Data added");
+			}, function() {
+				log("Error adding data");
+			});
+		});
 	});
+
 	function getViewport() {
 		return {
 			width: document.documentElement.clientWidth || window.innerWidth,
@@ -17,14 +43,14 @@
 
 	function init() {
 		resize();
-		$(window).on("resize",resize)
+		$(window).on("resize", resize)
 		//var hash=(location.hash || '#home').substr(1);
-		$.getJSON("javascript/database/module.json",function(data, textStatus, jqXHR){
+		$.getJSON("javascript/database/module.json", function(data, textStatus, jqXHR) {
 			var content = $("#nav-ul");
-			$.each(data, function(i,o) {
-				var item  = $("<li data-key="+o.data+"><a href='javascript:void(0)'>"+o.name+"</a></li>")
+			$.each(data, function(i, o) {
+				var item = $("<li data-key=" + o.data + "><a href='javascript:void(0)'>" + o.name + "</a></li>")
 				content.append(item);
 			});
 		});
 	}
-})(jQuery);	
+})(jQuery);
